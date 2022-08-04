1568ef96bdf8adb57855ebbaed4b523a796cf052
medhug <95834183+medhug@users.noreply.github.com>
2022-08-04 14:55:01 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import Landing from './pages/Landing/Landing';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    "user" : null,
+    pageDidMount : false,
+    isLoggedIn : false
+
+  }
+
+  componentDidMount(){
+    console.log("page mounted");
+    this.setState({
+      pageDidMount: true,
+    });
+
+  }
+
+  render (){
+    if(this.state.pageDidMount){
+      return (
+          <>
+          <BrowserRouter>
+            <Switch>
+              <h1>Page mounted and made it to APP.js</h1>
+              if(!this.state.isLoggedIn){
+                <Landing />
+              }
+              <Route exact path="/">
+                  <Redirect to="/dashboard" />
+              </Route>
+              <Route path="/signup" />
+              <Route path="/login" />
+              <Route path="/dashboard" />
+              <Route path="/learn" />
+              <Route path="/quiz" />
+              <Route path="/learn/:id" />
+              <Route path="/quiz/:id"  />
+            </Switch>
+          </BrowserRouter>
+          </>
+      );
+    } else {
+      return (
+        <>
+          <h2>page loading...</h2>
+        </>
+      )
+    }
+  }
+
 }
 
 export default App;
---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,9 @@
+function Landing() {
+    return (
+      <>
+        <h1>made it to landing</h1>
+      </>
+    );
+  }
+  
+  export default Landing;
