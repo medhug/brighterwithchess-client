f6013c32e756e1cbc8266409d22731fa68bdce66
medhug <95834183+medhug@users.noreply.github.com>
2022-08-05 16:26:40 -0400

    create form for sign up form

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,13 +1,50 @@
 import '../Signup/Signup.scss';
 
-function Signup() {
-    return (
-      <div className="signup">
-        <h1>made it to signup</h1>
-        
-        
-      </div>
-    );
-  }
-  
-  export default Signup;
+import { Component } from "react";
+import axios from "axios";
+import Input from "../../components/Input/Input";
+
+class Signup extends Component {
+    state = {
+        error: "",
+        success: false,
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios
+            .post("http://localhost:8080/api/users/register", {
+                email: event.target.email.value,
+                password: event.target.password.value,
+                first_name: event.target.first_name.value,
+            })
+            .then(() => {
+                this.setState({ success: true, error: "" });
+                event.target.reset();
+            })
+            .catch((error) => {
+                this.setState({ success: false, error: error.response ? error.response.data : error.message });
+            });
+    };
+
+    render() {
+        return (
+            <main className="signup-page">
+                <form className="signup" onSubmit={this.handleSubmit}>
+
+                    <Input type="text" name="email" label="Email" />
+                    <Input type="text" name="first_name" label="First name" />
+                    <Input type="password" name="password" label="Password" />
+                    <Input type="password2" name="password2" label="Repeat Password" />
+                    <button className="signup__button">Sign up</button>
+
+                    {this.state.success && <div className="signup__message">Signed up!</div>}
+                    {this.state.error && <div className="signup__message">{this.state.error}</div>}
+                </form>
+            </main>
+        );
+    }
+}
+
+export default Signup;
