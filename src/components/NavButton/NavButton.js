ced9a8b0f7aec15067bb9d3b3d1f6d4e416166c1
medhug <95834183+medhug@users.noreply.github.com>
2022-08-09 12:43:20 -0400

    signup and login buttons colors

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -1,10 +1,10 @@
 import "../NavButton/NavButton.scss";
-import addUser from '../../assets/icons/adduser.svg';
-import bolt from '../../assets/icons/bolt.svg';
-import cancel from '../../assets/icons/cancel.svg';
-import home from '../../assets/icons/home.svg';
-import login from '../../assets/icons/login.svg';
-import star from '../../assets/icons/star.svg';
+import SignupIcon from '../../assets/icons/adduser.svg';
+import QuizIcon from '../../assets/icons/bolt.svg';
+import CancelIcon from '../../assets/icons/cancel.svg';
+import HomeIcon from '../../assets/icons/home.svg';
+import LoginIcon from '../../assets/icons/login.svg';
+import LearnIcon from '../../assets/icons/star.svg';
 
 
 
@@ -13,6 +13,20 @@ function NavButton(props) {
     let label= props.label;
     let iconName;
     let text;
+
+    let table={
+        Home : HomeIcon,
+        Signup : SignupIcon,
+        Login : LoginIcon,
+        Dash : HomeIcon,
+        Learn : LearnIcon,
+        Quiz : QuizIcon,
+    }
+
+    console.log("the label is: ", label);
+    iconName= table[label];
+    console.log("name of icon", iconName);
+
     return (
         <main className="NavButton">
             <div className="NavButton-container">
