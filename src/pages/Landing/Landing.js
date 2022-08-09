e64d396d772a564e045b9a0ea2639ec9c486f52c
medhug <95834183+medhug@users.noreply.github.com>
2022-08-08 23:33:45 -0400

    landing info and styling

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -4,15 +4,23 @@ import chesshero from '../../assets/images/chess-hero.jpg';
 
 function Landing() {
     return (
-        <>
-            <div className="landing">
-                <h1>made it to landing</h1>
-                <h2> Chess is great because of reasons!</h2>
-                <div className="landing__hero-container">
-                    <img className="landing__hero-container--img" src={chesshero} alt="chess pieces by Felix Mittermeier unsplash" />
+        <main className="landing">
+            <div className="landing-container">
+                <h1 className="landing-container__title">Welcome to BrighterWithChess!</h1>
+                <div className="landing-container__hero-container">
+                    <img className="landing-container__hero-container--img" src={chesshero} alt="chess pieces by Felix Mittermeier unsplash" />
+                </div>
+                <p>Chess helps train and build a variety of mental skills.</p>
+                <p>These include:</p>
+                <p>Memory</p>
+                <p>Calculation</p>
+                <p>Pattern Recognition</p>
+
+                <div className="landing-container__about">
+                    <p className="landing-container__about--text">On this website you can learn each skill and take quizzes to test your new ability. Watch your progress grow as you complete exercises.</p>
                 </div>
             </div>
-      </>
+        </main>
     );
   }
   
