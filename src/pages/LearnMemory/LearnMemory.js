9c897828b10cc77230c72119b29b4250741c17c0
medhug <95834183+medhug@users.noreply.github.com>
2022-08-10 13:30:01 -0400

    learn page started

--- src/pages/LearnMemory/LearnMemory.js
+++ src/pages/LearnMemory/LearnMemory.js
@@ -1,29 +1,28 @@
-import '../LearnMemory/LearnMemory.scss';
-import {Component} from "react";
-import NotLoggedIn from '../../components/NotLoggedIn/NotLoggedin';
+import "../LearnMemory/LearnMemory.scss";
+import { Component } from "react";
+import NotLoggedIn from "../../components/NotLoggedIn/NotLoggedin";
+import bishop from "../../assets/images/bishop.jpg";
 
 class LearnMemory extends Component {
-
-  render(){
-
+  render() {
     console.log("from learn memory", this.props.userStatus);
 
-    if(this.props.userStatus === true){
+    if (this.props.userStatus === true) {
       return (
-          <>
+        <main className="dashboard">
+          <div className="dashboard-container">
             <h1>Lesson 1</h1>
-
-
-            
-          </>
-      );  
+            <h3>Let's memorize how the bishop moves.</h3>
+            <div>
+              <img src={bishop} alt="text" />
+            </div>
+          </div>
+        </main>
+      );
     } else {
-      return (
-        <NotLoggedIn/>
-      )
+      return <NotLoggedIn />;
     }
   }
-
 }
-  
+
 export default LearnMemory;
