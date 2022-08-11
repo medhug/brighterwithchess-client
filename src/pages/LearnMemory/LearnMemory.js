6304142604aa39dce4e88c87142054fd38d5ae8c
medhug <95834183+medhug@users.noreply.github.com>
2022-08-11 11:27:09 -0400

    learn styling and content

--- src/pages/LearnMemory/LearnMemory.js
+++ src/pages/LearnMemory/LearnMemory.js
@@ -13,12 +13,12 @@ class LearnMemory extends Component {
         <main className="learn-memory">
           <div className="learn-memory-container">
             <h1 className="learn-memory-container__title">Lesson 1</h1>
-            <h3 className="learn-memory-container__text">Let's meet the bishop:</h3>
+            <p className="learn-memory-container__text">Let's meet the bishop:</p>
             <div className="learn-memory-container__pictures">
               <img src={bishopdrawing} alt="text1" className="learn-memory-container__pictures--img" />
               <img src={bishopphoto} alt="text2" className="learn-memory-container__pictures--img"/>
             </div>
-            <h3 className="learn-memory-container__text">Let's memorize how the bishop moves.</h3>
+            <p className="learn-memory-container__text">Let's memorize how the bishop moves.</p>
             <div>
               {/* <img src={} alt="text3" /> */}
             </div>
