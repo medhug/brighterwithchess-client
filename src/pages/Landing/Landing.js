aa980ff8a17ae01e817ac9c24fd646c21fe4be9f
medhug <95834183+medhug@users.noreply.github.com>
2022-08-11 10:14:42 -0400

    clean up landing

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,27 +1,41 @@
 import "../Landing/Landing.scss";
-import chesshero from '../../assets/images/chess-hero.jpg';
-
+import chesshero from "../../assets/images/chess-hero.jpg";
 
 function Landing() {
-    return (
-        <main className="landing">
-            <div className="landing-container">
-                <h1 className="landing-container__title">Welcome to BrighterWithChess!</h1>
-                <div className="landing-container__hero-container">
-                    <img className="landing-container__hero-container--img" src={chesshero} alt="chess pieces by Felix Mittermeier unsplash" />
-                </div>
-                <p>Chess helps train and build a variety of mental skills.</p>
-                <p>These include:</p>
-                <p>Memory</p>
-                <p>Calculation</p>
-                <p>Pattern Recognition</p>
+  return (
+    <main className="landing">
+      <div className="landing-container">
+        <h1 className="landing-container__title">
+          Welcome to BrighterWithChess!
+        </h1>
+        <div className="landing-container__hero-container">
+          <img
+            className="landing-container__hero-container--img"
+            src={chesshero}
+            alt="chess pieces by Felix Mittermeier unsplash"
+          />
+        </div>
+        <div className="landing-container__about">
+          <p className="landing-container__about--text">
+            Chess can be used to train, build, and test a variety of mental
+            skills. These skills can include:
+          </p>
+          <ul className="landing-container__about--list">
+            <li>Memory</li>
+            <li>Calculation</li>
+            <li>Pattern Recognition</li>
+          </ul>
+          <p className="landing-container__about--text">
+            On this website you can learn each skill and take quizzes to test
+            your new ability.
+          </p>
+          <p className="landing-container__about--text">
+            Watch your progress grow as you complete exercises.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
 
-                <div className="landing-container__about">
-                    <p className="landing-container__about--text">On this website you can learn each skill and take quizzes to test your new ability. Watch your progress grow as you complete exercises.</p>
-                </div>
-            </div>
-        </main>
-    );
-  }
-  
-  export default Landing;
+export default Landing;
