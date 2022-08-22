7850f53522209e58f9157beb6e484c5712bdaf04
medhug <95834183+medhug@users.noreply.github.com>
2022-08-21 20:07:48 -0400

    learn section updated with capture lesson

--- src/pages/LearnMemory/LearnMemory.js
+++ src/pages/LearnMemory/LearnMemory.js
@@ -5,6 +5,8 @@ import bishopdrawing from "../../assets/images/BishopDrawing.png";
 import bishopphoto from "../../assets/images/BishopPhoto.JPG";
 import bishopmoves from "../../assets/images/bishopsmoves.jpg";
 import bishopmoved from "../../assets/images/bishopmoved.jpg";
+import bishopwillcapture from "../../assets/images/bishopwillcapture.jpg";
+import bishopcaptured from "../../assets/images/bishopcaptured.jpg";
 
 class LearnMemory extends Component {
   render() {
@@ -24,14 +26,23 @@ class LearnMemory extends Component {
             <div className="learn-memory-container__movement">
               <img src={bishopmoves} alt="text3" className="learn-memory-container__movement--img" />
             </div>
-            <p className="learn-memory-container__text">The bishop moves diagonally, like an X.</p>
+            <p className="learn-memory-container__text">The bishop moves diagonally, like an X (above).</p>
             <p className="learn-memory-container__text">You can choose any one of the squares with a black dot.</p>
             <p className="learn-memory-container__text">It can move as many or a few squares as you want.</p>
             <p className="learn-memory-container__text">Note: The bishop cannot jump over other pieces.</p>
             <div className="learn-memory-container__movement">
-              <img src={bishopmoved} alt="text3" className="learn-memory-container__movement--img" />
+              <img src={bishopmoved} alt="text4" className="learn-memory-container__movement--img" />
             </div>
-            <p className="learn-memory-container__text">Did you see the bishop's new position?</p>
+            <p className="learn-memory-container__text">Did you see the bishop's new position (above)?</p>
+            <p className="learn-memory-container__text">Let's memorize how the bishop captures, or takes, a piece.</p>
+            <div className="learn-memory-container__movement">
+              <img src={bishopwillcapture} alt="text5" className="learn-memory-container__movement--img" />
+            </div>
+            <p className="learn-memory-container__text">The bishop captures the black rook(the castle) by replacing the rook on it's square.</p>
+            <div className="learn-memory-container__movement">
+              <img src={bishopcaptured} alt="text3" className="learn-memory-container__movement--img" />
+            </div>
+            <p className="learn-memory-container__text">The rook has been captured (above).</p>
           </div>
         </main>
       );
